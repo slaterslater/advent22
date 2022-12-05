deed47a993009a704f8f2a7624e3efc01986eed0
slaterslater <slaterslater@gmail.com>
2022-12-05 13:35:54 -0500

    completes day 5

---
+++ day5/part1.js
@@ -0,0 +1,29 @@
+const input = require("fs").readFileSync("./input").toString('utf-8').split("\n")
+
+const supplies = input.slice(0,8).reduce((stacks, row) => {
+  for (let i = 0; i < 9; i++){
+    const crate = row[i*4+1].replace(' ', '')
+    stacks[i] += crate
+ }
+  return stacks
+}, Array.from({length:9}).fill(''))
+
+const procedure = input.slice(10).map(procedure => 
+  procedure
+    .match(/\d+/g)
+    .map(digit => parseInt(digit))
+)
+
+procedure.forEach((step) => {
+  let [n, f, t] = step
+  while (n) {
+    const [from, to] = [f,t].map(i => supplies[i - 1])
+    supplies[t - 1] = from[0] + to
+    supplies[f - 1] = from.slice(1)
+    n--
+  }
+})
+
+const topCrates = supplies.map(stack => stack[0]).join('')
+
+console.log(topCrates)
